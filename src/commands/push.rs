9a01ec60c2df6a6aef36295e23aa4880e3d60ca2
Fredrik Jagenheim <fjagenheim@jabra.com>
2023-04-13 16:03:34 +0200

    push now does push, list does list

--- src/commands/push.rs
+++ src/commands/push.rs
@@ -4,12 +4,12 @@ use crate::{
 };
 
 impl AdbTcpConnexion {
-    /// Pushes
+    /// Pushes [filename] to [path] on the device.
     pub fn push_command<S: ToString>(
         &mut self,
         serial: Option<S>,
-        _filename: S,
-        _path: S,
+        filename: S,
+        path: S,
     ) -> Result<()> {
         self.new_connection()?;
 
@@ -24,8 +24,32 @@ impl AdbTcpConnexion {
         // Set device in SYNC mode
         Self::send_adb_request(&mut self.tcp_stream, AdbCommand::Sync)?;
 
+        // Send a send command
+        Self::send_sync_request(
+            &mut self.tcp_stream,
+            SyncCommand::Send(&filename.to_string(), path.to_string()),
+        )?;
+
+        Ok(())
+    }
+
+    /// Lists files in [path] on the device.
+    pub fn list_command<S: ToString>(&mut self, serial: Option<S>, path: S) -> Result<()> {
+        self.new_connection()?;
+
+        match serial {
+            None => Self::send_adb_request(&mut self.tcp_stream, AdbCommand::TransportAny)?,
+            Some(serial) => Self::send_adb_request(
+                &mut self.tcp_stream,
+                AdbCommand::TransportSerial(serial.to_string()),
+            )?,
+        }
+
+        // Set device in SYNC mode
+        Self::send_adb_request(&mut self.tcp_stream, AdbCommand::Sync)?;
+
         // Send a list command
-        Self::send_sync_request(&mut self.tcp_stream, SyncCommand::List("/data/"))?;
+        Self::send_sync_request(&mut self.tcp_stream, SyncCommand::List(&path.to_string()))?;
 
         Ok(())
     }
